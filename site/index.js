76b53b11c4c8c831356bb262e1a3e9c68864e3e8
Olga-Yezerska <olga_ezerska06@ukr.net>
2025-03-23 21:45:12 +0200

    Add files via upload

---
+++ site/index.js
@@ -0,0 +1,45 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+    let filePath = '.' + req.url;
+
+    if (req.url === '/' || req.url === '/index') {
+        filePath = './index.html';
+    }
+
+    // Визначаємо Content-Type залежно від розширення файлу
+    const extname = path.extname(filePath);
+    let contentType = 'text/html';
+    
+    switch (extname) {
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.css':
+            contentType = 'text/css';
+            break;
+        case '.jpg':
+        case '.jpeg':
+            contentType = 'image/jpeg';
+            break;
+        case '.png':
+            contentType = 'image/png';
+            break;
+    }
+
+    fs.readFile(filePath, (err, data) => {
+        if (err) {
+            res.writeHead(404, { 'Content-Type': 'text/html' });
+            res.end('<h1>404 Not Found</h1>');
+        } else {
+            res.writeHead(200, { 'Content-Type': contentType });
+            res.end(data);
+        }
+    });
+});
+
+server.listen(8080, () => {
+    console.log('Server running at http://localhost:8080');
+});
